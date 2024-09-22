a827c30b6331288d7eac5a71b6c246f07efd3b90
ivansaul <saul_2503@hotmail.com>
2024-09-21 19:19:37 -0500

    chore: initial commit

---
+++ SwiftUIMovieDB/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIMovieDB
+//
+//  Created by saul on 9/21/24.
+//
+//  https://github.com/ivansaul
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMovieDB/SwiftUIMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieDBApp.swift
+//  SwiftUIMovieDB
+//
+//  Created by saul on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
